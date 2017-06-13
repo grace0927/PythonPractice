903aed51f8f2eb2d8ca39ab3559971b228af7939
hearst-feng <jfeng@hearst.com>
2017-06-13 16:10:36 -0400

    2017-06-13
    the hard way 5 & 6

---
+++ the_hard_way/ex5.py
@@ -0,0 +1,17 @@
+name = 'Jianyu Feng'
+age = 30
+height = 74
+weight = 180
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+print "Let's talk about %s" % name
+print "He's %d inches tall" % height
+print "He's %d pounds heavy" % weight
+print "Actually that's not too heavy"
+print "Hes got %s eyes and %s hair." % (eyes, hair)
+print "His teeth are usually %s depending on the coffee." % teeth
+
+print "If I add %d, %d, and %d I get %d." % (
+    age, height, weight, age + height + weight)
