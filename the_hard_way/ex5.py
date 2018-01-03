151424442e6551f957964f8905f58615a0030b3d
Jianyu Feng <cellphone2008@gmail.com>
2018-01-03 10:06:17 -0500

    change to py3
    use py3

--- the_hard_way/ex5.py
+++ the_hard_way/ex5.py
@@ -1,17 +1,18 @@
-name = 'Jianyu Feng'
-age = 30
-height = 74
-weight = 180
-eyes = 'Blue'
-teeth = 'White'
-hair = 'Brown'
+my_name = 'Jianyu Feng'
+my_age = 30
+my_height = 74
+my_weight = 180
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
 
-print "Let's talk about %s" % name
-print "He's %d inches tall" % height
-print "He's %d pounds heavy" % weight
-print "Actually that's not too heavy"
-print "Hes got %s eyes and %s hair." % (eyes, hair)
-print "His teeth are usually %s depending on the coffee." % teeth
+print(f"Let's talk about {my_name}")
+print(f"He's {my_height} inches tall")
+print(f"He's {my_weight} pounds heavy")
+print("Actually that's not too heavy")
+print(f"Hes got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffee.")
 
-print "If I add %d, %d, and %d I get %d." % (
-    age, height, weight, age + height + weight)
+# this line is tricky, try to get it exactly right
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
--- the_hard_way/ex7.py
+++ the_hard_way/ex7.py
@@ -1,7 +1,7 @@
-print "Mary had a little lamb."
-print "Its fleece was white as %s." % 'snow'  # replace string
-print "And everywhere that Mary went."
-print "." * 10   # print 10 '.'
+print("Mary had a little lamb.")
+print("Its fleece was white as {}.".format('snow'))  # replace string
+print("And everywhere that Mary went.")
+print("." * 10)   # print 10 '.'
 
 end1 = "C"
 end2 = "h"
@@ -18,5 +18,5 @@ end12 = "r"
 
 
 # watch that comma at the end. removing it make it two lines.
-print end1 + end2 + end3 + end4 + end5 + end6,
-print end7 + end8 + end9 + end10 + end11 + end12
+print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')
+print(end7 + end8 + end9 + end10 + end11 + end12)
