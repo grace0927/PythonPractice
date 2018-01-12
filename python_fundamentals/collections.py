12543a49f1fe635a9d41a7838e495d8671eddb74
Jianyu Feng <cellphone2008@gmail.com>
2018-01-12 14:16:11 -0500

    collections exec 2

--- python_fundamentals/collections.py
+++ python_fundamentals/collections.py
@@ -1,5 +1,12 @@
+from pprint import pprint as pp
+
+
+def print_title(title):
+    half_banner = '*'*10
+    print(half_banner + title + half_banner)
+
 def tuple_practice():
-    print("tuple Practice")
+    print_title('tuple')
 
     t = ("Norway", 4.342, 4)
 
@@ -15,7 +22,7 @@ def tuple_practice():
     print(4 in t)
 
 def str_practice():
-    print("str Practice")
+    print_title('str')
 
     m = ' '.join(['hello', 'world'])
     print(m)
@@ -26,6 +33,8 @@ def str_practice():
     print('{hello}, {world}'.format(hello=hello, world=world))
 
 def range_practice():
+    print_title('range')
+
     for i in range(5):
         print(i)
 
@@ -41,6 +50,8 @@ def range_practice():
         print("k={}, v={}".format(k, v))
 
 def list_practice():
+    print_title('list')
+
     m = "hello world my friends are here".split(' ')
     print(m[-1])
 
@@ -59,8 +70,62 @@ def list_practice():
     k[0].append(1)
     print(v)
 
+def dict_practice():
+    print_title('dict')
+
+    city = {'China': 'Beijing',
+            'USA': 'Washington'}
+    print(city['USA'])
+
+    for value in city.values():
+        print(value)
+
+    nba = dict(Lakers='Los Angeles', Bulls='Chicago', Knicks="New York")
+    copy = nba.copy()
+    print(copy)
+
+    nba.update(Celtics='Boston')
+    texas = dict(Rockets='Hoston', Spurs='San Antonio', Mavericks='Dallas')
+    nba.update(texas)
+    print(nba)
+
+    for key in nba:
+        print('nba team {key} is in city {value}'.format(key=key, value=nba[key]))
+
+    for team, city in nba.items():
+        print(f'City {city} has nba team {team}')
+
+    print('Lakers' in nba)
+    print('Look' in nba)
+    pp(nba)
+
+def set_practice():
+    print_title('set')
+
+    nba_city = {'Los Angeles', 'Dallas', 'Chicago', 'New York'}
+    print(type(nba_city))
+
+    team = set(['Lakers', 'Spurs', 'Mavericks'])
+    print(team)
+
+    team.add('Rockets')
+    team.update(['Rockets', 'Bulls'])
+    print(team)
+
+    west_team = {'Lakers', 'Kings', 'Rockets'}
+    ca_team = {'Lakers', 'Kings', 'Clippers'}
+    taxes_team = {'Rockets'}
+    print(west_team.union(ca_team))
+    print(west_team.intersection(ca_team))
+    print(west_team.difference(ca_team))
+    print(west_team.symmetric_difference(ca_team))
+    print(taxes_team.issubset(west_team))
+    print(west_team.issuperset(west_team))
+    print(ca_team.isdisjoint(taxes_team))
 
 tuple_practice()
 str_practice()
 range_practice()
 list_practice()
+dict_practice()
+set_practice()
