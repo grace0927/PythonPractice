601bce2a35c3ef67e28ed6144b6f772b54a2a821
Jianyu Feng <cellphone2008@gmail.com>
2018-01-15 21:39:01 -0500

    exception exec #2

--- python_fundamentals/exceptional.py
+++ python_fundamentals/exceptional.py
@@ -1,11 +1,12 @@
-'''A module for demostrating exceptions.'''
+"""A module for demostrating exceptions."""
 
+import os
 import sys
 from math import log
 
 
 def convert(s):
-    '''Convert to an integer.'''
+    """Convert to an integer."""
     try:
         return int(s)
     except (ValueError, TypeError) as e:
@@ -16,6 +17,17 @@ def convert(s):
 def string_log(s):
     x = convert(s)
     return log(x)
+
+def make_at(path, dir_name):
+    original_path = os.getcwd()
+    try:
+        os.chdir(path)
+        os.mkdir(dir_name)
+    except OSError as e:
+        print(e, sys.stderr)
+        raise
+    finally:
+        os.chdir(original_path)
 
 # convert(3)
 # convert("33")
